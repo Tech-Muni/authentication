e68a2215847ef83bf301d5645c245534bae6b94f
Tech-Muni <77907692+Tech-Muni@users.noreply.github.com>
2021-05-24 11:48:44 +0530

    Add files via upload
    using bcrypt for authentication

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-const app = express();
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
-console.log(process.env.API_KEY);
+const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -23,10 +23,6 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
-
-
 const User = new mongoose.model("User", userSchema);
 
 app.get("/", function (req, res) {
@@ -40,15 +36,19 @@ app.get("/register", function (req, res) {
 });
 
 app.post("/register", function (req, res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password,
-  });
-  newUser.save(function (err) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    if (!err) {
+      const newUser = new User({
+        email: req.body.username,
+        password: hash,
+      });
+      newUser.save(function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          res.render("secrets");
+        }
+      });
     }
   });
 });
@@ -61,9 +61,11 @@ app.post("/login", function (req, res) {
       console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === password) {
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if(result === true){
           res.render("secrets");
-        }
+          }
+      });
       }
     }
   });
